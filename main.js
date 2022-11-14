0a213d337f1805f0a879a08c79605dc1bfd7b6c8
anikapatel1881 <102267255+anikapatel1881@users.noreply.github.com>
2022-11-14 17:52:55 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//4. Set the function preload as an empty function:
+function preload(){
+
+}
+
+
+
+function setup() {
+    //5. Set the characteristics for the canvas and the video
+     canvas = createCanvas(700, 500);
+     canvas.position(65, 300)
+     video = createCapture(VIDEO);
+     video.hide();
+    //6. Declare the variable tint_color with an empty value
+    tint_color = "";
+
+}
+
+
+function draw() {
+
+    //7. Set the parameters for the image and the color filter:
+    image(video,0, 0, 700, 500);
+    tint(tint_color);
+}
+
+function take_snapshot(){    
+  save('student_name.png');
+}
+
+function filter_tint()
+{
+    //8. Assign the value of tint_color according to the color the user sets on the input component: 
+	tint_color = document.getElementById("color_name").value;
+}
+
